98a24bc9e6682b7c60ffc5b7d6906f1c477e6aa0
Benjamin-Harris1 <beha1503@gmail.com>
2023-09-05 23:29:54 +0200

    Changes

--- frontend/rest.js
+++ frontend/rest.js
@@ -2,6 +2,14 @@
 
 const endpoint = "http://localhost:3000";
 
+import { showArtists } from "./script.js";
+
+async function updateArtistGrid() {
+  const artists = await readArtists();
+  //const filteredList = filterList(artists);
+  showArtists(artists);
+}
+
 async function readArtists() {
   const response = await fetch(`${endpoint}/artists`);
   const data = await response.json();
@@ -12,6 +20,104 @@ async function createArtist(event) {
   event.preventDefault();
   console.log("Create");
   const form = event.target;
-    const name = form.name.value;
-    
+  const name = form.name.value;
+  const birthdate = form.birthdate.value;
+  const activeSince = form.activeSince.value;
+  const label = form.label.value;
+  const website = form.website.value;
+  const genres = form.genres.value;
+  const shortDescription = form.shortDescription.value;
+  const image = form.image.value;
+
+  const newArtist = {
+    name,
+    birthdate,
+    activeSince,
+    label,
+    website,
+    genres,
+    shortDescription,
+    image,
+  };
+
+  const artistJSON = JSON.stringify(newArtist);
+  const response = await fetch(`${endpoint}/artists`, {
+    method: "POST",
+    body: artistJSON,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    updateUsersGrid();
+  }
 }
+
+async function deleteArtist(id) {
+  const response = await fetch(`${endpoint}/artists/${id}`, {
+    method: "DELETE",
+  });
+  if (response.ok) {
+    updateArtistGrid();
+  }
+}
+
+// TJEK LIGE MERE OP PÅ DEN HER
+function artistClicked(artist) {
+  document.querySelector("#dialog-update-artist").showModal();
+
+  artistClicked = artist;
+
+  const form = document.querySelector("#form-update-artist");
+
+  form.name.value = artist.name;
+  form.birthdate.value = artist.birthdate;
+  form.activeSince.value = artist.activeSince;
+  form.label.value = artist.label;
+  form.website.value = artist.website;
+  form.genres.value = artist.genres;
+  form.shortDescription.value = artist.shortDescription;
+  form.image.value = artist.image;
+
+  document.querySelector("#form-update-artist").addEventListener("submit", updateArtist);
+}
+
+async function updateArtist(event) {
+  event.preventDefault();
+  console.log("Update");
+
+  const form = event.target;
+  const name = form.name.value;
+  const birthdate = form.birthdate.value;
+  const activeSince = form.activeSince.value;
+  const label = form.label.value;
+  const website = form.website.value;
+  const genres = form.genres.value;
+  const shortDescription = form.shortDescription.value;
+  const image = form.image.value;
+
+  const updatedArtist = {
+    name,
+    birthdate,
+    activeSince,
+    label,
+    website,
+    genres,
+    shortDescription,
+    image,
+  };
+
+  const artistJSON = JSON.stringify(updatedArtist);
+  const response = await fetch(`${endpoint}/artists/${artistClicked.id}`, {
+    method: "PUT",
+    body: artistJSON,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    updateArtistGrid();
+  }
+}
+
+export { updateArtist, createArtist, deleteArtist, artistClicked, updateArtistGrid };
--- frontend/script.js
+++ frontend/script.js
@@ -1,26 +1,24 @@
 "use strict";
 
-
-
 window.addEventListener("load", initApp);
+
+import { updateArtistGrid, createArtist, deleteArtist, artistClicked, updateArtist } from "./rest.js";
 
 async function initApp() {
   console.log("js is working");
-  await updateUsersGrid();
-  document.querySelector("#searchbar").addEventListener("keyup", inputSearchChanged);
-  document.querySelector("#searchbar").addEventListener("search", inputSearchChanged);
-  showRole(document.querySelector("#coach-label"), "træner", "coach", "Vælg træner", users);
-  showRole(document.querySelector("#coach-update-label"), "træner", "coach", "Vælg træner", users);
+  await updateArtistGrid();
+  //document.querySelector("#searchbar").addEventListener("keyup", inputSearchChanged);
+  //document.querySelector("#searchbar").addEventListener("search", inputSearchChanged);
 
-  document.querySelector("#create-user-btn").addEventListener("click", showCreateUserDialog);
-  document.querySelector("#form-create-user").addEventListener("submit", createUserClicked);
-  document.querySelector("#sortByRole").addEventListener("change", filterByMemberRoles);
+  document.querySelector("#create-artist-btn").addEventListener("click", showCreateUserDialog);
+  document.querySelector("#form-create-artist").addEventListener("submit", createArtistClicked);
+  //document.querySelector("#sortByRole").addEventListener("change", filterByMemberRoles);
 
-  document.querySelector("#sortByTeam").addEventListener("change", teamSelect);
+  //document.querySelector("#sortByTeam").addEventListener("change", teamSelect);
 }
 
 // Search
-function inputSearchChanged(event) {
+/*function inputSearchChanged(event) {
   const input = event.target.value;
   const listOfUsers = searchUsers(input);
   showUsers(listOfUsers);
@@ -33,64 +31,69 @@ function searchUsers(search) {
   return results;
 }
 
-async function updateUsersGrid() {
-  users = await getUsers();
-  const filteredList = filterList();
-  showUsers(filteredList);
-}
-
 function filterList() {
   const filteredList = getAllRole(users, filterOption);
   const filteredTeamList = filterByTeam(filteredList);
 
   return filteredTeamList;
-}
+}*/
 
 function showCreateUserDialog() {
-  document.querySelector("#dialog-create-user").showModal();
+  document.querySelector("#dialog-create-artist").showModal();
 }
 
-async function createUserClicked(event) {
+async function createArtistClicked(event) {
   const form = event.target;
-  const firstName = form.firstName.value;
-  const lastName = form.lastName.value;
-  const age = form.age.value;
-  const gender = form.gender.value;
-  const subscription = form.subscription.value;
-  const role = form.role.value;
-  const discipline = form.discipline.value;
-  const coach = form.coach.value;
+  const name = form.name.value;
+  const birthdate = form.birthdate.value;
+  const activesince = form.activeSince.value;
+  const genres = form.genres.value;
+  const website = form.website.value;
+  const shortDescription = form.shortDescription.value;
   const image = form.image.value;
-  const debt = form.debt.value;
   form.reset();
-  const response = await createUser(role, subscription, discipline, age, coach, firstName, lastName, debt, gender, image);
+  const response = await createArtist(name, birthdate, activesince, genres, website, shortDescription, image);
   if (response.ok) {
     showSnackbar("Bruger oprettet");
-    updateUsersGrid();
+    updateArtistGrid();
   } else {
     console.log(response.status, response.statusText);
     showSnackbar("Noget gik galt. Prøv igen");
   }
 }
 
-function showUsers(listOfUsers) {
-  console.log(listOfUsers);
-  document.querySelector("#users").innerHTML = "";
-  listOfUsers.sort((a, b) => {
-    if (a.firstName.toLowerCase() < b.firstName.toLowerCase()) return -1;
-    else if (a.firstName.toLowerCase() > b.firstName.toLowerCase()) return 1;
-    else return 0;
-  });
-  for (const users of listOfUsers) {
-    showUser(users); // for every post object in listOfPosts, call showPost
+function showArtists(artists) {
+  console.log(artists);
+  document.querySelector("#artists").innerHTML = "";
+  for (const artist of artists) {
+    const html = /*html*/ `
+    <article class="grid-item-user">
+  <img src="${artist.image}">
+  <h3>${artist.name} </h3>
+  <p>${artist.birthdate}</p>
+<p>${artist.genres}</p>
+<p>${artist.shortDescription}</p>
+
+  <div class="btns">
+  <button class="btn-favorite">Mark as favorite</button>
+  <button class="btn-update">Update</button>
+  <button class="btn-delete">Delete</button>
+  </div>
+</article>
+`;
+
+    document.querySelector("#artists").insertAdjacentHTML("beforeend", html);
+    document.querySelector("#artists article:last-child .btn-delete").addEventListener("click", () => deleteClicked(artist));
+    document.querySelector("#artists article:last-child .btn-update").addEventListener("click", () => updateClicked(artist));
+    document.querySelector("#artists article:last-child img").addEventListener("click", () => showUserModal(artist));
   }
 }
 
-function filterByMemberRoles(event) {
+/*function filterByMemberRoles(event) {
   const role = event.target.value;
   filterOption = role;
   console.log(filterOption);
-  updateUsersGrid();
+  updateArtistGrid();
 }
 
 function filterByTeam(list) {
@@ -107,108 +110,37 @@ function filterByTeam(list) {
 function teamSelect(event) {
   teamOption = event.target.value;
 
-  updateUsersGrid();
+  update;
   console.log(teamOption);
+}*/
+
+function updateClicked(artist) {
+  document.querySelector("#dialog-update-artist").showModal();
+  document.querySelector("#update-name").value = artist.name;
+  document.querySelector("#update-birthdate").value = artist.birthdate;
+  document.querySelector("#update-activeSince").value = artist.activeSince;
+  document.querySelector("#update-genres").value = artist.genres;
+  document.querySelector("#update-label").value = artist.label;
+  document.querySelector("#update-website").value = artist.website;
+  document.querySelector("#update-image").value = artist.image;
+  document.querySelector("#update-shortDescription").value = artist.shortDescription;
+
+  document.querySelector("#form-update-artist").addEventListener("submit", updateArtistClicked);
 }
 
-function showUser(userObject) {
-  //dom manipulation
-  const html = /*html*/ `
-<article class="grid-item-user">
-  <img src="${userObject.image}">
-  <h3>${userObject.firstName} ${userObject.lastName} </h3>
-  <h3>${userObject.age} år</h3>
-  <div class="btns">
-  <button class="btn-update">Opdater</button>
-  <button class="btn-delete">Slet</button>
-  </div>
-</article>
-`;
-  document.querySelector("#users").insertAdjacentHTML("beforeend", html);
-  document.querySelector("#users article:last-child .btn-delete").addEventListener("click", () => deleteClicked(userObject));
-  document.querySelector("#users article:last-child .btn-update").addEventListener("click", () => updateClicked(userObject));
-  document.querySelector("#users article:last-child img").addEventListener("click", () => showUserModal(userObject));
-}
-
-function updateClicked(userObject) {
-  document.querySelector("#dialog-update-user").showModal();
-  document.querySelector("#update-firstName").value = userObject.firstName;
-  document.querySelector("#update-lastName").value = userObject.lastName;
-  document.querySelector("#update-age").value = userObject.age;
-  document.querySelector("#update-gender").value = userObject.gender;
-  document.querySelector("#update-subscription").value = userObject.subscription;
-  document.querySelector("#update-role").value = userObject.role;
-  document.querySelector("#discipline-output").textContent = userObject.discipline;
-  // document.querySelector(`#${userObject.coachId}`);
-  for (const coach of document.querySelectorAll(`#form-update-user .træner${userObject.coachId}`)) {
-    coach.selected = true;
-  }
-  document.querySelector("#update-debt").value = userObject.debt;
-  document.querySelector("#update-image").value = userObject.image;
-  document.querySelector("#form-update-user").setAttribute("data-id", userObject.id);
-  document
-    .querySelector("#extra-discipline-btn-update")
-    .addEventListener("click", () =>
-      addDisciplineToOutput(document.querySelector("#update-discipline"), document.querySelector("#discipline-output"))
-    );
-  document
-    .querySelector("#remove-disciplines-btn-update")
-    .addEventListener("click", () => removeDisciplineToOutput(document.querySelector("#discipline-output")));
-  document.querySelector("#form-update-user").addEventListener("submit", updateUserClicked);
-}
-function addDisciplineToOutput(disciplineSelector, outputSelector) {
-  console.log("addToOutput");
-  const disciplineValue = disciplineSelector.value;
-  if (disciplineValue != "" && outputSelector.textContent.includes(disciplineValue) == false)
-    if (outputSelector.textContent != "") outputSelector.textContent += ", " + disciplineValue;
-    else outputSelector.textContent += disciplineValue;
-}
-function removeDisciplineToOutput(outputSelector) {
-  outputSelector.textContent = "";
-}
-
-function deleteClicked(userObject) {
-  console.log("Knappen Virker");
-  document.querySelector("#dialog-delete-user").showModal();
-  document.querySelector("#dialog-delete-user-name").textContent = userObject.firstName;
-  document.querySelector("#form-delete-user").setAttribute("data-id", userObject.id);
-  document.querySelector("#btn-no").addEventListener("click", function () {
-    document.querySelector("#dialog-delete-user").close();
-  });
-  document.querySelector("#form-delete-user").addEventListener("submit", deleteUserClicked);
-}
-
-async function deleteUserClicked(event) {
-  event.preventDefault();
+async function updateArtistClicked(event) {
   const form = event.target;
   const id = form.getAttribute("data-id");
-  const response = await deleteUser(id);
-  if (response.ok) {
-    updateUsersGrid();
-    showSnackbar("Bruger slettet");
-  } else {
-    console.log(response.status, response.statusText);
-    showSnackbar("Noget gik galt. Prøv igen");
-  }
-  form.reset();
-  document.querySelector("#dialog-delete-user").close();
-}
-
-async function updateUserClicked(event) {
-  const form = event.target;
-  const id = form.getAttribute("data-id");
-  const firstName = form.firstName.value;
-  const lastName = form.lastName.value;
-  const age = form.age.value;
-  const gender = form.gender.value;
-  const subscription = form.subscription.value;
-  const role = form.role.value;
-  const discipline = form.discipline.value;
-  const coach = form.coach.value;
+  const name = form.name.value;
+  const birthdate = form.birthdate.value;
+  const activeSince = form.activeSince.value;
+  const genres = form.genres.value;
+  const label = form.label.value;
+  const website = form.website.value;
   const image = form.image.value;
-  const debt = form.debt.value;
+  const shortDescription = form.shortDescription.value;
   form.reset();
-  const response = await updateUser(id, role, subscription, discipline, age, coach, firstName, lastName, debt, gender, image);
+  const response = await updateArtist(id, name, birthdate, activeSince, genres, label, website, image, shortDescription);
   if (response.ok) {
     showSnackbar("Bruger opdateret");
     updateUsersGrid();
@@ -219,20 +151,40 @@ async function updateUserClicked(event) {
   console.log("knappen virker");
 }
 
-function showUserModal(user) {
-  document.querySelector("#dialog-age").textContent = user.age + " år gammel";
-  document.querySelector("#dialog-name").textContent = `${user.firstName} ${user.lastName}`;
-  document.querySelector("#dialog-subscription").textContent = capitalizeFirstLetter(user.subscription);
-  document.querySelector("#dialog-role").textContent = capitalizeFirstLetter(user.role);
-  if (document.querySelector("#dialog-discipline").textContent != "")
-    document.querySelector("#dialog-discipline").textContent = capitalizeFirstLetter(user.discipline);
-  else document.querySelector("#dialog-member-info > section > section > ul > li:nth-child(3)").hidden;
-  document.querySelector("#dialog-gender").textContent = capitalizeFirstLetter(user.gender);
-  document.querySelector("#dialog-image").src = user.image;
+function deleteClicked(artist) {
+  console.log("Knappen Virker");
+  document.querySelector("#dialog-delete-artist").showModal();
+  document.querySelector("#dialog-delete-artist-name").textContent = artist.name;
+  document.querySelector("#form-delete-artist").setAttribute("data-id", artist.id);
+  document.querySelector("#btn-no").addEventListener("click", function () {
+    document.querySelector("#dialog-delete-artist").close();
+  });
+  document.querySelector("#form-delete-artist").addEventListener("submit", deleteUserClicked);
+}
 
-  console.log("");
+async function deleteUserClicked(event) {
+  event.preventDefault();
+  const form = event.target;
+  const id = form.getAttribute("data-id");
+  const response = await deleteArtist(id);
+  if (response.ok) {
+    updateUsersGrid();
+    showSnackbar("Bruger slettet");
+  } else {
+    console.log(response.status, response.statusText);
+    showSnackbar("Noget gik galt. Prøv igen");
+  }
+  form.reset();
+  document.querySelector("#dialog-delete-artist").close();
+}
+
+function showUserModal(artist) {
+  document.querySelector("#dialog-name").textContent = artist.name;
+  document.querySelector("#dialog-genre").textContent = artist.genre;
+  document.querySelector("#dialog-birthdate").textContent = artist.birthdate;
+  document.querySelector("#dialog-image").src = artist.image;
   // show dialog
-  document.querySelector("#dialog-member-info").showModal();
+  document.querySelector("#dialog-artist-info").showModal();
 }
 
 function showSnackbar(message) {
@@ -243,6 +195,5 @@ function showSnackbar(message) {
     snackbarSelector.classList.remove("show");
   }, 3000);
 }
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+
+export { showArtists };
